05dc382648858a86dee7655536f15a80e8439d59
Michael Wang <mwang@splunk.com>
2015-08-13 01:27:35 -0700

    Implement client-side email and password validation

--- public/js/controllers/wildhacks.js
+++ public/js/controllers/wildhacks.js
@@ -1,5 +1,32 @@
 var wildhacks = angular.module('wildhacks', []);
 
-wildhacks.controller('FormCtrl', ['$scope', function($scope) {
-  $scope.text = "Hello world!";
-}])
+wildhacks.controller('RegisterCtrl', ['$scope', function($scope) {
+  $scope.submit = function() {
+    alert("goteeeem");
+  };
+  
+}]);
+
+angular.module('wildhacks').directive('equals', function() {
+  return {
+    //restrict: 'A',  // attribute only
+    require: '?ngModel',  // get ngModelController
+    link: function(scope, elem, attrs, ngModel) {
+      if (!ngModel) return;
+      
+      scope.$watch(attrs.ngModel, function() {
+        validate();
+      });
+      
+      attrs.$observe('equals', function(val) {
+        validate();
+      });
+      
+      var validate = function() {
+        var val1 = ngModel.$viewValue;
+        var val2 = attrs.equals;
+        ngModel.$setValidity('equals', !val1 || !val2 || val1 === val2);
+      };
+    }
+  };
+});
