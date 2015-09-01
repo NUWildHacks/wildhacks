7f25ecfdee767a5a7d31e759f6b8679677d7c9f6
skorlir <skorlir@gmail.com>
2015-09-01 18:35:09 -0500

    Simplify and remove passport, mongoose, etc

--- config/app.js
+++ config/app.js
@@ -1,37 +1,26 @@
-var passport       = require('passport')
-  , expressSession = require('express-session')
+var expressSession = require('express-session')
   , cookieParser   = require('cookie-parser')
   , methodOverride = require('method-override')
   , path           = require('path')
-  , favicon        = require('favicon')
   , logger         = require('morgan')
   , bodyParser     = require('body-parser')
   , express        = require('express')
-  , User           = require('../models/user.js')
 
 module.exports = function (app) {
   // view engine setup
   app.set('views', __dirname + '/../public/views');
   app.engine('.html', require('ejs').renderFile);
 
-  // app.use(favicon(__dirname + '/public/favicon.ico'));
   app.use(logger('dev'));
 
   // NOTE(Jordan): Request data parsing
   app.use(cookieParser())
   app.use(methodOverride())
   app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
   // Configuring Passport
-  app.use(expressSession({secret: 'mySecretKey'}));
-  app.use(passport.initialize());
-  app.use(passport.session());
-
-  // Initialize Passport
-  passport.use(User.createStrategy());
-  passport.serializeUser(User.serializeUser());
-  passport.deserializeUser(User.deserializeUser());
+  app.use(expressSession({ secret: process.env.SESSION_SECRET }));
 
   app.use(express.static(path.join(__dirname, '../public')));
 }
--- config/example.js
+++
@@ -1,4 +0,0 @@
-// config/db.js
-// module.exports = {
-//   'url': 'mongodb://<user>:<password>@ds035713.mongolab.com:35713/wildhacks_users'
-// };
