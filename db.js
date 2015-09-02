d1ddb3242e2d7a8a4424379beb1eb9be22bedbd3
skorlir <skorlir@gmail.com>
2015-09-01 20:38:51 -0500

    Add LevelUP and configure it to use databases in /db

---
+++ db.js
@@ -0,0 +1,12 @@
+var leveup = require('level')
+
+var env = process.env.NODE_ENV
+  , db
+
+if (env == 'production') {
+  db = levelup('../db/prod')
+} else {
+  db = levelup('../db/dev')
+}
+
+module.exports = db
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,30 @@
 // routes/server.js
-var express  = require('express');
-var router   = express.Router();
+var express = require('express')
+  , router  = express.Router()
+  , db      = require('../db.js')
 
-// GET main page
 router.get('/', function(req, res) {
   res.render('index.html')
+});
+
+router.get('/apply', function (req, res) {
+  res.render('application.html')
+});
+
+router.get('/application-session/:hash', function (req, res) {
+  var key = req.params.hash
+  db.get(key, function (err, value) {
+    if (err) res.send({})
+    else res.send(value);
+  })
+});
+
+router.put('/application-session/:hash', function (req, res) {
+  var key = req.params.hash
+  db.put(key, req.body, function (err, value) {
+    if(err) console.error(err)
+    else res.send('okay!')
+  })
 });
 
 module.exports = router;
