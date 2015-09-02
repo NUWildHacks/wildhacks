b503fbb23ce05b18287157eb659498a43ffa8fbc
skorlir <skorlir@gmail.com>
2015-09-01 22:25:44 -0500

    Redirect to /apply#{email}:{hash}

--- public/js/controllers/wildhacks.js
+++ public/js/controllers/wildhacks.js
@@ -3,17 +3,17 @@ var wildhacks = angular.module('wildhacks', []);
 wildhacks.controller('RegisterCtrl', ['$scope', '$http', '$window', function($scope, $http, $window) {
   // if true, display the login page, otherwise display the registration page
   $scope.showRegister = true;
-  
+
   $scope.authenticate = function() {
     var email = $scope.user.email;
-    
+
     var key = sjcl.codec.utf8String.toBits($scope.user.password);
     var out = (new sjcl.misc.hmac(key, sjcl.hash.sha256)).mac($scope.user.email);
     var hmac = sjcl.codec.hex.fromBits(out)
-    
-    var url = "#" + email + ":" + hmac;
+
+    var url = "/apply#" + email + ":" + hmac;
     $window.location.href = url;
-    
+
   };
 }]);
 
@@ -24,15 +24,15 @@ wildhacks.directive('equals', function() {
     require: '?ngModel',  // get ngModelController
     link: function(scope, elem, attrs, ngModel) {
       if (!ngModel) return;
-      
+
       scope.$watch(attrs.ngModel, function() {
         validate();
       });
-      
+
       attrs.$observe('equals', function(val) {
         validate();
       });
-      
+
       var validate = function() {
         var val1 = ngModel.$viewValue;
         var val2 = attrs.equals;
